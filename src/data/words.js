d79e3d66b4baa1a6e7a4178d25eb5a91d9ab1e59
Ondrej Maksi <ondrej.maksi@gmail.com>
2026-01-17 13:53:00 +0100

    Fix vybrané slová po B a M

--- src/data/words.js
+++ src/data/words.js
@@ -1,13 +1,13 @@
 // Vybrané slová po B a M
 export const VYBRANE_SLOVA = [
   // Po B
-  "by", "aby", "koby", "čoby", "akoby", "toby",
+  "by", "aby", "keby", "akoby",
   "bystrý", "bylina", "býk", "bývať", "bývalý",
   "kobyla", "bydlo", "obyčaj", "obyvateľ", "dobytok",
-  "zbytočný", "nábytek", "príbytok", "dobývať",
+  "zbytočný", "nábytok", "príbytok", "dobývať", "bytosť",
   // Po M
   "my", "vy", "mýliť", "myš", "mydlo", "myseľ",
   "mýto", "myšlienka", "hmyz", "priemysel", "úmysel",
-  "výmysel", "pomykov", "zamykať", "vymýšľať", "omyl",
-  "mykať", "šmýkať", "chmýrie", "žmýkať"
+  "výmysel", "zamykať", "vymýšľať", "omyl",
+  "mykať", "šmýkať", "chmýrie", "žmýkať", "pomyje"
 ];
